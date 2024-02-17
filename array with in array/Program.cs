86e79ddd406f25e984c04c2e82ce3ba2f488094a
ayush <ayush@AYUSHGANDHI>
2024-02-17 10:40:51 +0530

    Add project files.

---
+++ array with in array/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace array_with_in_array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[][] ayu=new string[2][];
+            ayu[0] = new string[3];
+            ayu[1] = new string[4];
+
+            ayu[0][0] = "Vadodara";
+            ayu[0][1] = "Rajkot";
+            ayu[0][2] = "Ahemdabad";
+
+            ayu[1][0] = "Gujarat";
+            ayu[1][1] = "Madhya Pradesh";
+            ayu[1][2] = "Uttar pradesh";
+            ayu[1][3] = "Maharastra";
+
+            foreach(var inarray in ayu)
+            {
+                foreach (var item in inarray)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
